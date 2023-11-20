e463048f4916db8e4b664c2a33b4e0ecdc8019aa
Jhonanthan Campos <dionathancampos@hotmail.com>
2023-11-19 21:24:50 -0300

    Implementação findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,12 +12,14 @@ public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
 		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		//Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
 		//Forma de injeção de dependencia sem o programa conhecer a implementação 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(seller);
+		Seller sellerDao2 = sellerDao.findById(3);
+		
+		System.out.println(sellerDao2);
 
 	}
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao{
 
+	//Preparando uma dependência para todos os métodos
+	private Connection conex;
+	public SellerDaoJDBC(Connection conex) {
+		this.conex = conex;
+	}
+	
 	@Override
 	public void insert(Seller obj) {
 		
@@ -27,8 +40,46 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public Seller findById(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
 		
-		return null;
+		try {
+			st = conex.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ " FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id = ?");
+					
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			
+			if(rs.next()) {
+				Department dp = new Department();
+				dp.setId(rs.getInt("DepartmentId"));
+				dp.setName(rs.getString("DepName"));
+				
+				Seller obj = new Seller();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setDepartment(dp);
+				
+				return obj;
+			}
+			return null;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			// não precisa fechar a conexão pois ela pode ser reutilizada nos outros metodos
+			// então fecha a conexão no programa principal
+		}
 	}
 
 	@Override
